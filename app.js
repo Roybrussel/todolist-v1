e053ef57e4b731204154ac16d1ce2ec06c78e202
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-01-22 22:50:04 +0100

    Adding tasks is working.

--- app.js
+++ app.js
@@ -3,38 +3,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var tasks = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  var date = new Date();
-  var currentDay = date.getDay();
-  var day = "";
+  var today = new Date();
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  res.render("list", { kindOfDay: day });
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, listOfTasks: tasks });
+});
+
+app.post("/", function (req, res) {
+  tasks.push(req.body.task);
+  console.log(tasks);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
