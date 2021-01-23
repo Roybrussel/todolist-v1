f961ee3e8505869090033c4c4f3c6f5e82bd678e
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-01-23 22:12:00 +0100

    Moved date code to date.js, exported it and required it into app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -12,16 +13,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date();
   res.render("list", { listTitle: day, listOfTasks: tasks });
 });
 
