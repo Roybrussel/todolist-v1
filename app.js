944ef455a17726186cabcc22d6f92f773ee80a83
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-02-02 12:16:06 +0100

    Created a to-do list database.

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 
 const app = express();
-
-// Getting document ready for Mongoose
-
-let tasks = [];
-let workList = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const tasksSchema = {
+  name: String,
+};
+
+const Task = mongoose.model("task", tasksSchema);
+
+// const task1 = new Task({
+//   name: "Welcome to your to-do list!",
+// });
+
+// const task2 = new Task({
+//   name: "Hit the + buton to add a new item.",
+// });
+
+// const task3 = new Task({
+//   name: "<-- Hit this to delete an item.",
+// });
+
+// const defaultTasks = [task1, task2, task3];
+
+// Task.insertMany(defaultTasks, function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Succesfully saved default tasks to database.");
+//   }
+// });
+
 app.get("/", function (req, res) {
-  let day = date();
-  res.render("list", { listTitle: day, listOfTasks: tasks });
+  Task.find({}, function (err, foundTasks) {
+    res.render("list", { listTitle: "Today", listOfTasks: foundTasks });
+  });
 });
 
 app.post("/", function (req, res) {
